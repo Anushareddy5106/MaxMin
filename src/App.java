7f75ad865363e6dcd4fed3a4c94c66d007460839
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-26 14:08:12 +0530

    Generic method for multiple values

--- src/App.java
+++ src/App.java
@@ -2,9 +2,9 @@ public class App {
 
     public static void main(String[] args) throws Exception {
 
-        FindMax<Integer> obj1 = new FindMax<Integer>(20, 10, 30);
-        FindMax<Float> obj2 = new FindMax<Float>(20.3f, 10.1f, 10.4f);
-        FindMax<String> obj3 = new FindMax<String>("Apple", "Banana", "Peach");
+        FindMax<Integer> obj1 = new FindMax<Integer>(20, 10, 32, 85, 72, 98, 30);
+        FindMax<Float> obj2 = new FindMax<Float>(20.3f, 10.1f, 105.4f, 45.9f, 90.3f);
+        FindMax<String> obj3 = new FindMax<String>("Apple", "Orange", "Banana", "Grape", "Kiwi");
 
         System.out.println(obj1.find());
         System.out.println(obj2.find());
--- src/FindMax.java
+++ src/FindMax.java
@@ -1,32 +1,27 @@
+import java.util.Arrays;
+
 public class FindMax<T extends Comparable<T>> {
 
-    public T val1;
-    public T val2;
-    public T val3;
+    public T[] values;
 
-    FindMax(T val1, T val2, T val3) {
-        this.val1 = val1;
-        this.val2 = val2;
-        this.val3 = val3;
+    FindMax(T... values) {
+        this.values = values;
     }
 
     public T find() {
-        return FindMax.find(val1, val2, val3);
+        return FindMax.find(values);
     }
 
-    public static <T extends Comparable<T>> T find(T val1, T val2, T val3) {
-        System.out.print("Maximum of " + val1 + ", " + val2 + ", " + val3 + " : ");
-        T max = val1;
+    public static <T extends Comparable<T>> T find(T... values) {
+        System.out.print("Maximum of the given : ");
 
-        if (val2.compareTo(max) > 0) {
-            max = val2;
+        if (values == null || values.length == 0) {
+            System.out.println("At least one value must be provided");
         }
 
-        if (val3.compareTo(max) > 0) {
-            max = val3;
-        }
+        Arrays.sort(values);
+        return values[values.length - 1];
 
-        return max;
     }
-
+    
 }
