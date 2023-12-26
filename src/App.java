abb7c1b38c4ee162e8aafdf66534af051814c616
Anushareddy5106 <anushachandupatla.2003@gmail.com>
2023-12-26 12:47:48 +0530

    Float-MaxMin

--- src/App.java
+++ src/App.java
@@ -1,10 +1,27 @@
 public class App {
 
+    // UC1
     public static Integer findMax(Integer num1, Integer num2, Integer num3) {
 
         System.out.println("Maximum of " + num1 + ", " + num2 + ", " + num3 + " is");
-        Integer max = num1.compareTo(num2);
-        if (max == 1)
+        Integer max;
+        if (num1.compareTo(num2) == 1)
+            max = num1;
+        else
+            max = num2;
+
+        if (max.compareTo(num3) == -1)
+            max = num3;
+
+        return max;
+    }
+
+    // UC2
+    public static Float findMax(Float num1, Float num2, Float num3) {
+
+        System.out.println("Maximum of " + num1 + ", " + num2 + ", " + num3 + " is");
+        Float max;
+        if (num1.compareTo(num2) == 1)
             max = num1;
         else
             max = num2;
@@ -18,6 +35,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
+        // UC1
         System.out.println("TC 1:");
         System.out.println(findMax(20, 10, 5));
 
@@ -26,6 +44,16 @@ public class App {
 
         System.out.println("TC 3:");
         System.out.println(findMax(20, 10, 45));
+
+        // UC2
+        System.out.println("TC 1:");
+        System.out.println(findMax(20.4f, 10.3f, 5.2f));
+
+        System.out.println("TC 2:");
+        System.out.println(findMax(20.8f, 30.1f, 5.02f));
+
+        System.out.println("TC 3:");
+        System.out.println(findMax(20f, 20.3f, 20.1f));
 
     }
 }
